75ea715bd245b534c3ba23c51f2074708e35ce19
luisoliva <luisalbertolivamedina@hotmail.com>
2019-10-25 12:23:56 -0500

    app routing implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {VistaGeneralComponent} from './vista-general/vista-general.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "vistaGeneral",
+    component: VistaGeneralComponent
+  },
+  {
+    path: "**",
+    redirectTo: "vistaGeneral"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
